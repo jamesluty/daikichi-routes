581c8311abbc5172c67fed25699e472960860c11
James Luty <luty0125@gmail.com>
2022-06-10 13:51:00 -0700

    Added new routes

--- src/main/java/com/jamesluty/daikichiroutes/controllers/DaikichiController.java
+++ src/main/java/com/jamesluty/daikichiroutes/controllers/DaikichiController.java
@@ -1,6 +1,7 @@
 package com.jamesluty.daikichiroutes.controllers;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -20,5 +21,21 @@ public class DaikichiController {
 	@GetMapping("/tomorrow")
 	public String tomorrow() {
 		return "Tomorrow, an opportunity will arise, so be sure to be open to new ideas!";
+	}
+	
+	@GetMapping("/travel/{location}")
+	public String travel(@PathVariable("location") String location) {
+		return "Congratulations! You will soon travel to " + location + ".";
+	}
+	
+	@GetMapping("/lotto/{num}")
+	public String lotto(@PathVariable("num") Integer num) {
+		String output = "";
+		if(num%2 == 0) {
+			output = "You will take a grand journey in the near future.";
+		} else {
+			output = "You have enjoyed the fruits of your lable but now is a great time to spend time with family and friends.";
+		}
+		return output;
 	}
 }
